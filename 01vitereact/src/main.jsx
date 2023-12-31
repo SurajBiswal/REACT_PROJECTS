0256932132804c580b571331dd906e9b0b6b7fd4
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2023-12-31 17:25:05 +0530

    jsx
    jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,7 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+const x = "xyz";
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank' },
+  'click me to visit google'
+) 
+
+// const domElement = document.createElement(reactElement.type)
+//     domElement.innerHTML = reactElement.children
+//     for(const prop in reactElement.props){
+//         if(prop ==='children') continue;
+//         domElement.setAttribute(prop, reactElement.props[prop])
+//         container.appendChild(domElement)
+//     }
+
+    
+
 ReactDOM.createRoot(document.getElementById('root')).
 render(
-  <App />
+    reactElement
 )
