6e60801701505fc8b83f7a5cbab3d5a58c436f24
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2024-01-04 23:33:15 +0530

    custom hooks

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='text-3xl bg-orange-500'>Cuuerncy converter</h2>
+    </>
+  )
+}
+
+export default App
