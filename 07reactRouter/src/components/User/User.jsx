5e973ffafef06ba10c5bfaf0ca1c14381064df42
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2024-02-20 14:59:19 +0530

    .

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function User() {
+  const { userid } = useParams();
   return (
-    <div>
-      User:
-    </div>
-  )
+    <div className="bg-gray-600 text-white text-3xl p-4">User:{userid}</div>
+  );
 }
 
-export default User
+export default User;
---
+++ 08minicontext/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
+
+function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUser } = useContext(UserContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(), setUser({ userName, password });
+  };
+  return (
+    <div>
+      <h2>Log In</h2>
+      <input
+        type="text"
+        value={userName}
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+        placeholder="userName"
+      />{" "}
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        placeholder="password"
+      />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
